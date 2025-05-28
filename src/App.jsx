a367746e40d11299c0476e5b9303024e2572f0c4
CodieMisiani <nimrodmisiani1@gmail.com>
2025-05-28 06:51:41 +0300

    chore: remove unused imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,11 @@ import TransactionForm from "./components/TransactionForm/TransactionForm";
 
 import Header from "./components/Header/Header.jsx";
 
-
 const App = () => {
   return (
-<<<<<<< HEAD
-    <>
-      <Header />
-    </>
-=======
     <div>
       <TransactionForm />
     </div>
->>>>>>> feat/transaction-form
   );
 };
 
